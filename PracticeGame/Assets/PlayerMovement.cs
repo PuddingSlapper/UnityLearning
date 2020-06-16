c9c4f4dc6845e98a3888b4075e3aa44e150be788
PuddingSlapper <rajanadirhabib@gmail.com>
2020-06-16 13:42:54 +0500

    Movement vid
    got dat sidebooty

--- PracticeGame/Assets/PlayerMovement.cs
+++ PracticeGame/Assets/PlayerMovement.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-
+    public float ForwardForce = 2000f;
+    public float SidewaysForce = 500f;
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+        
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(SidewaysForce * Time.deltaTime , 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
