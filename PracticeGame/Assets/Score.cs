9003ea8e78cde6fd94efdfc89fe6d259263adf2a
PuddingSlapper <rajanadirhabib@gmail.com>
2020-06-16 17:16:33 +0500

    Score UI
    Some scoring

---
+++ PracticeGame/Assets/Score.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text ScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text=(player.position.z+55.6).ToString("0");
+    }
+}
